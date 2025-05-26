bb6acff94b6730948da90d04f58e74d2ae3f2367
Parthib Mukherjee <parthibmukherjee@gmail.com>
2025-05-26 18:31:02 +0000

    refactor(movies): edited the showMovieHandler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,7 +14,9 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 
-	id, err := app.readIDParam(r)
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
